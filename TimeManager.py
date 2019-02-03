2fd7a25f6d5778c9a6407c9f865130f789134324
Wallace Carr <wallacecarr4@gmail.com>
2019-02-02 22:31:14 -0700

    update TimeManager conversion

--- TimeManager.py
+++ TimeManager.py
@@ -1,7 +1,6 @@
 import calendar, time, datetime
 
 class TimeManager:
-
     def __init__(self):
         pass
 
@@ -25,20 +24,10 @@ class TimeManager:
         return epoch_time
 
     def convertEpochToTime(self, epochInput):
-        # timeResult = time.strftime('%m-%d-%Y %H:%M:%S', time.localtime(epoch))
         timeResult = time.strftime('%d.%m.%Y %H:%M:%S', time.localtime(epochInput))
         return timeResult
 
     def convertTimeToEpoch(self, dateTime):
-        # date_time = '29.08.2011 11:05:02'
-        # pattern = '%d-%m-%Y %H:%M:%S'
-        # pattern = '%H:%M:%S'
-        # epochResult = time.mktime(time.strptime(timeInput, pattern));#int(time.mktime(time.strptime(timeInput, pattern)))
-        # return epochResult
-
         pattern = '%d.%m.%Y %H:%M:%S'
         epoch = int(time.mktime(time.strptime(dateTime, pattern)))
         return epoch
-
-        # epochResult = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch))
-        # return epochResult
