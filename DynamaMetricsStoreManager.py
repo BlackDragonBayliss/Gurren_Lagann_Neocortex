e34e2b2f13f6a856686d541a8a75dac7278eeca4
Wallace Carr <wallacecarr4@gmail.com>
2019-02-23 19:45:26 -0700

    add Dynama support

---
+++ DynamaMetricsStoreManager.py
@@ -0,0 +1,11 @@
+
+class DynamaMetricsStoreManager:
+    def __init__(self):
+        cortexicalThought = []
+
+    def storeMetrics(self):
+        pass
+
+    def createCustomStore(self):
+        pass
+
