d480f29222649514f620c5d8c9744a426185df32
Wallace Carr <wallacecarr4@gmail.com>
2019-03-21 22:36:37 -0600

    Update GoldenGooseDeterminer

--- API.py
+++ API.py
@@ -17,10 +17,7 @@ def init_system():
 
 @app.route('/recordQueries', methods=['POST'])
 def recordQueries():
-
-
     content = request.get_json()
-    # print(content)
     return_value = 'pass'
     for key, value in content.items():
         # print("hit bird intake")
@@ -31,36 +28,8 @@ def recordQueries():
                         operationCenter = OperationCenter()
                         operationCenter.goldenGooseProcess(value)
 
-
-                        # for key, value in value.items():
-                        #     if key == "data":
-                        #         # print(value)
-
-
-                                # operation_center = Operation_Center()
-                                # operation_center.process_async_top_stock_phase1_internal()
-                                # return_value = 'res'
     return "Query received"
 
-# @app.route('/birdMessengerQuery', methods=['POST'])
-# def birdMessengerQuery():
-#     # content = request.get_json()
-#     # print(content)
-#     content = request.get_json()
-#     return_value = 'pass'
-#     for key, value in content.items():
-#         print("hit bird intake")
-#         if key == 'request_type':
-#             if value == "bird1":
-#                 for key, value in content.items():
-#                     if key == "payload":
-#                         for key, value in value.items():
-#                             if key == "data":
-#                                 operation_center = Operation_Center()
-#                                 operation_center.process_async_top_stock_phase1_internal()
-#                                 return_value = 'res'
-#
-#     return "birdMessengerQuery received"
 
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0', port=4440)
